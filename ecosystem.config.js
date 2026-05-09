bce4559e4b4f4338dc29bfc1ab7b82cc4b19c95c
Xizh994 <623701305@qq.com>
2026-05-09 19:32:49 +0800

    chore: 更新ecosystem.config.js从服务器

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,40 +1,40 @@
 module.exports = {
-  apps: [
-    {
-      name: 'dazistar',
-      cwd: '/www/dazistar',
-      script: 'server.js',
-      args: '--port 3000',
-      instances: 1,
-      exec_mode: 'fork',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      },
-      error_file: '/www/dazistar/logs/dazistar-3000-err.log',
-      out_file: '/www/dazistar/logs/dazistar-3000-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true
-    },
-    {
-      name: 'dazistar-3001',
-      cwd: '/www/dazistar',
-      script: 'server.js',
-      args: '--port 3001',
-      instances: 1,
-      exec_mode: 'fork',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      },
-      error_file: '/www/dazistar/logs/dazistar-3001-err.log',
-      out_file: '/www/dazistar/logs/dazistar-3001-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true
-    }
-  ]
-}
+ apps: [
+ {
+ name: 'dazistar',
+ cwd: '/www/dazistar',
+ script: 'server.js',
+ args: '--port 3000',
+ instances: 1,
+ exec_mode: 'fork',
+ autorestart: true,
+ watch: false,
+ max_memory_restart: '1G',
+ env: {
+ NODE_ENV: 'production'
+ },
+ error_file: '/www/dazistar/logs/dazistar-3000-err.log',
+ out_file: '/www/dazistar/logs/dazistar-3000-out.log',
+ log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+ merge_logs: true
+ },
+ {
+ name: 'dazistar-3001',
+ cwd: '/www/dazistar-test',
+ script: 'server.js',
+ args: '--port 3001',
+ instances: 1,
+ exec_mode: 'fork',
+ autorestart: true,
+ watch: false,
+ max_memory_restart: '1G',
+ env: {
+ NODE_ENV: 'production'
+ },
+ error_file: '/www/dazistar/logs/dazistar-3001-err.log',
+ out_file: '/www/dazistar/logs/dazistar-3001-out.log',
+ log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+ merge_logs: true
+ }
+ ]
+};
